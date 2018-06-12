dbbbb76ce17480360d246dd5f8538b64e75dba76
bdangla-senorcoders <brian@senorcoders.com>
2018-06-12 17:54:56 -0600

    add video background on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from'../services/product.service';
 import { AuthenticationService } from '../services/authentication.service';
-
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,10 +11,13 @@ export class HomeComponent implements OnInit {
   products:any;
   API:string="http://138.68.19.227:7000";
   user:any;
+  video:string="http://seafood.senorcoders.com/Water Sea Ocean.mp4";
+  videoURLSafe:any;
   onResize(event) {
     this.setHeight(event.target.innerHeight);
   }
-  constructor(private product:ProductService, private auth: AuthenticationService) { }
+  constructor(private product:ProductService, private auth: AuthenticationService, private sanitizer:DomSanitizer) {
+  this.videoURLSafe = sanitizer.bypassSecurityTrustResourceUrl(this.video); }
   setHeight(h){
     document.getElementById("hero").style.height = h+"px";
   }
