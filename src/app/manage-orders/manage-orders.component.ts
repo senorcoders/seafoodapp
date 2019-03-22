3cdc57356c1fe855df9a5c140a09695eed01d3b4
Dayana Lara <dayana@seafoodsouq.com>
2019-03-22 15:22:14 -0600

    Add const API to manage-orders

--- src/app/manage-orders/manage-orders.component.ts
+++ src/app/manage-orders/manage-orders.component.ts
@@ -24,6 +24,7 @@ export class ManageOrdersComponent implements OnInit {
   showNoData: boolean = false;
   groupOrder=[];
   orderWithData=[];
+  API:any = environment.apiURL;
   constructor(
     private orderService: OrderService,
     private productService: ProductService,
