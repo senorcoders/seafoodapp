fc8b883ec77ae3e79cf5b0778264155e14edf8be
dlara-ss <dayana@seafoodsouq.com>
2019-04-05 16:20:14 -0600

    Change button hover design for all new pages

--- src/app/my-products/my-products.component.ts
+++ src/app/my-products/my-products.component.ts
@@ -36,6 +36,8 @@ export class MyProductsComponent implements OnInit {
      }
 
   ngOnInit() {
+    
+
     this.user = this.auth.getLoginData();
     this.getCurrentPricingCharges();
     this.getMyData();
@@ -74,6 +76,8 @@ export class MyProductsComponent implements OnInit {
     if (this.user.role === 0) {
       this.productService.getData('store/allProducts').subscribe(result => {
         this.products = result;
+        setTimeout(() =>  this.replaceImgBySvg(), 1000);
+
         // working on the images to use like background
         this.products.forEach((data, index) => {
           if (data.imagePrimary && data.imagePrimary !== '') {
@@ -89,6 +93,8 @@ export class MyProductsComponent implements OnInit {
       this.productService.getData('store/' + this.store.id).subscribe(result => {
         this.products = result['fishs'];
         console.log("Products seller", this.products);
+        setTimeout(() =>  this.replaceImgBySvg(), 1000);
+
         // working on the images to use like background
         this.products.forEach((data, index) => {
           if (data.imagePrimary && data.imagePrimary !== '') {
@@ -102,6 +108,44 @@ export class MyProductsComponent implements OnInit {
       });
     }
 
+  }
+
+
+  replaceImgBySvg(){
+    jQuery('img.icon-action').each(function(){
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+  
+      jQuery.get(imgURL, function(data) {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find('svg');
+  
+          // Add replaced image's ID to the new SVG
+          if(typeof imgID !== 'undefined') {
+              $svg = $svg.attr('id', imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if(typeof imgClass !== 'undefined') {
+              $svg = $svg.attr('class', imgClass+' replaced-svg');
+          }
+  
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr('xmlns:a');
+          
+          // Check if the viewport is set, else we gonna set it if we can.
+          if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+              $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+          }
+       
+          
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+  
+      }, 'xml');
+  
+  });
   }
 
   smallDesc(str) {
