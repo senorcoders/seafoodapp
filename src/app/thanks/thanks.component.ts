d9f1fdd2fa991217fb8976123b72516dd36d6517
dlara-ss <dayana@seafoodsouq.com>
2019-04-23 15:55:59 -0600

    Thanks Design

--- src/app/thanks/thanks.component.ts
+++ src/app/thanks/thanks.component.ts
@@ -8,39 +8,12 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./thanks.component.scss']
 })
 export class ThanksComponent implements OnInit {
-  loadProduct:boolean=true;
-  products:any = [];
-  API:string=environment.apiURLImg;
+
 
   constructor(private product:ProductService) { }
 
   ngOnInit() {
-    this.getComingsoon();
   }
 
-  getComingsoon(){
-    let json = {
-      "category":"0","subcategory":"0","subspecies":"0","descriptor":"0","country":"0","raised":[],"preparation":[],"treatment":[],"minPrice":"0","maxPrice":"0","minimumOrder":"0","maximumOrder":"0","cooming_soon":"true"};
-    console.log(json);
-    this.product.saveData('fish/filter', json).subscribe(
-      result=>{
-        console.log("prod", result);
-        this.products = result;
-        this.loadProduct = false;
-      },
-      error=>{
-        console.log(error);
-      }
-    )
-  }
 
-  getShortDesc(data){  
-    if(data.length>20){
-        let text=data.split(/\s+/).slice(0,20).join(" ")
-        return text+'...' 
-    }
-    else{
-      return data
-    }
-  }
 }
