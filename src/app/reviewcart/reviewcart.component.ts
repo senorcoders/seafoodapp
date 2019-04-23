2af87801a84510380272f397d37c4a6dd98becbe
dlara-ss <dayana@seafoodsouq.com>
2019-04-23 16:56:59 -0600

    fix merge

--- src/app/reviewcart/reviewcart.component.ts
+++ src/app/reviewcart/reviewcart.component.ts
@@ -9,20 +9,13 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./reviewcart.component.scss']
 })
 export class ReviewcartComponent implements OnInit {
-  products: any = [];
-  total: any;
-  totalOtherFees: any;
-  shipping: any;
-  totalWithShipping: any;
-  buyerId: string;
+
   shoppingCartId: any;
-  userInfo:any;
 
   constructor(
-    private orders: OrderService,
     private route: ActivatedRoute,
     private router: Router,
-    private auth: AuthenticationService
+   
 
 
   ) { }
@@ -30,43 +23,11 @@ export class ReviewcartComponent implements OnInit {
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.shoppingCartId = params['shoppingCartId'];
-      this.userInfo = this.auth.getLoginData();
-      this.buyerId = this.userInfo['id'];
-      this.getCart();
-
     });
 
   }
 
-  getCart() {
-   this.orders.getCart(this.buyerId)
-          .subscribe(
-            res => {
-              if (res && res['items'] !== '') {
-              this.products = res['items'];
 
-              console.log(res);
-              this.total = res['subTotal'];
-              this.shipping = res['shipping'];
-              this.totalOtherFees = res['totalOtherFees'] + res['uaeTaxes'];
-              this.totalWithShipping = res['total'];
-              localStorage.setItem('shippingCost', this.shipping);
-              localStorage.setItem('shoppingTotal', this.totalWithShipping);
-              localStorage.setItem('shoppingCartId', this.shoppingCartId);
-              localStorage.setItem('totalOtherFees', this.totalOtherFees);
-            }
-          
-            },
-            error => {
-              console.log(error);
-            }
-          );
-  
-  }
-
-  getTotalxItem(count, price) {
-    return count * price;
-  }
 
   goToNextPage(){
         this.router.navigate(['/checkout'],  {queryParams: {shoppingCartId: this.shoppingCartId}});
