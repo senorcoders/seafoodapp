17b160c928c6d37bcb4f4e20bb2fbcc0eb322260
dlara-ss <dayana@seafoodsouq.com>
2019-05-02 09:33:55 -0600

    Add swapsie

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -7,6 +7,7 @@ declare var jQuery;
 })
 export class RegistrationFormComponent implements OnInit {
 buyer:boolean = true;
+class:any = '';
   constructor() { }
 
   ngOnInit() {
@@ -19,32 +20,34 @@ buyer:boolean = true;
     console.log("Switching");
     
     if(this.buyer == true){
-      this.buyer = false;
-      jQuery('.side-dark-gray:parent').each(function(){
-        jQuery(this).addClass('sliding');
-      })
-      jQuery('.side-dark-gray:parent')
-      .animate({right: "+=80%" }, 1000);
-      jQuery('.gray-light-side:parent').animate({left: "+=20%"}, 1000)
-      setTimeout(() => {
-        jQuery('.side-dark-gray:parent').each(function () {
-          jQuery(this).animate({right: '0px'}, "linear")
-          jQuery('.gray-light-side:parent').animate({left: "0px"}, "linear");
-          jQuery(this).removeClass('sliding');
-          jQuery(this).insertBefore(jQuery(this).prev('.gray-light-side'));
-          jQuery('#dark-side-title').html('WANTO TO BUY?');
-          jQuery('#dark-side-p').html('Want to get the best seafood for the lowest price? Register below!');
-          jQuery('#dark-side-btn').html("I'm a buyer");
-      });
-      }, 1001);
+      this.buyer = false;  
+      jQuery("#content_dark").swap({  
+        target: "content_light", // Mandatory. The ID of the element we want to swap with  
+        opacity: "0.8", // Optional. If set will give the swapping elements a translucent effect while in motion  
+        speed: 1200, // Optional. The time taken in milliseconds for the animation to occur  
+        callback: function() { // Optional. Callback function once the swap is complete  
+            console.log("Swap Complete");  
+                jQuery('#dark-side-title').html('WANTO TO BUY?');
+                jQuery('#dark-side-p').html('Want to get the best seafood for the lowest price? Register below!');
+                jQuery('#dark-side-btn').html("I'm a buyer");
+        }  
+    });
+      
     }else{
       this.buyer = true;
-      jQuery('.gray-light-side:parent').each(function () {
-        jQuery(this).insertBefore(jQuery(this).prev('.side-dark-gray'));
-        jQuery('#dark-side-title').html('WANTO TO sell?');
-        jQuery('#dark-side-p').html("Well, what are you waiting for? That fish isn't going to sell itself...");
-        jQuery('#dark-side-btn').html("I'm a seller");
-    });
+       jQuery("#content_dark").swap({  
+         target: "content_light", // Mandatory. The ID of the element we want to swap with  
+         opacity: "0.8", // Optional. If set will give the swapping elements a translucent effect while in motion  
+         speed: 1200, // Optional. The time taken in milliseconds for the animation to occur  
+         callback: function() { // Optional. Callback function once the swap is complete  
+             console.log("Swap Complete");  
+                    jQuery(this).insertBefore(jQuery(this).prev('.side-dark-gray'));
+                   jQuery('#dark-side-title').html('WANTO TO sell?');
+                  jQuery('#dark-side-p').html("Well, what are you waiting for? That fish isn't going to sell itself...");
+                  jQuery('#dark-side-btn').html("I'm a seller");
+         }  
+     });
+   
     }
  
 
