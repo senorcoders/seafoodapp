abb701a760a69921443df9dae8f15fdc3e63df71
dlara-ss <dayana@seafoodsouq.com>
2019-05-15 09:50:49 -0600

    Users should be anchored to the validation error on forms

--- src/app/edit-account/edit-account.component.ts
+++ src/app/edit-account/edit-account.component.ts
@@ -218,9 +218,27 @@ export class EditAccountComponent implements OnInit {
       this.updateAccount(this.info);
     }else{
       this.validateAllFormFields(this.buyerForm);
+      this.scrollToError();
     }
   }
 
+  scrollTo(el: Element): void {
+    if(el) { 
+      console.log("el", el);
+     el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+ }
+ 
+ scrollToError(): void {
+   let that:any = this;
+  setTimeout(function(){
+    const firstElementWithError = document.querySelector('.is-invalid');
+    console.log("HTMLElement", firstElementWithError);
+    that.scrollTo(firstElementWithError);
+   }, 500);
+
+ 
+ }
   updateSeller(){
     if(this.sellerForm.valid){
       console.log("Valido");
@@ -245,6 +263,7 @@ export class EditAccountComponent implements OnInit {
       this.updateAccount(this.info);
     }else{
       this.validateAllFormFields(this.sellerForm);
+      this.scrollToError();
     }
   }
 
