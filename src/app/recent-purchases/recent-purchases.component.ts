d084235f3b81463c89180d0a0009af5f6e119ed3
dlara-ss <dayana@seafoodsouq.com>
2019-05-20 12:56:52 -0600

    Remove vars from dom

--- src/app/recent-purchases/recent-purchases.component.ts
+++ src/app/recent-purchases/recent-purchases.component.ts
@@ -91,7 +91,7 @@ export class RecentPurchasesComponent implements OnInit {
     $mainNav.append("<li id='magic-line'></li>");
 
     jQuery('#magic-line').css('position', 'absolute');
-    jQuery('#magic-line').css('background', '$dark_blue');
+    jQuery('#magic-line').css('background', '#094D82');
     jQuery('#magic-line').css('height', '2px');
     jQuery('#magic-line').css('width', '280px');
     jQuery('#magic-line').css('left', '0');
