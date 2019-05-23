4759cc4dc3696d72b15c9dcc0a63c3a7a66a1f35
Osmany Jirón <osmany@senorcoders.com>
2019-05-23 11:01:23 -0600

    jquery y bootstrap se cargan con el cdn

--- src/app/cdn-check.ts
+++ src/app/cdn-check.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 // import 'whatwg-fetch'
 import { environment } from "../environments/environment";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Script } from "vm";
 
 @Injectable()
 export class CDNCheck {
@@ -10,17 +9,16 @@ export class CDNCheck {
     cdn = "./"
 
     constructor(private http: HttpClient) {
-        // new Script("https://d66z88a3oqo8j.cloudfront.net/assets/cdn/select2.min.js", { timeout: 3000, displayErrors: true }).runInThisContext();
     }
 
     async load() {
-        
+
         await this.awaitCDNResponse();
 
         //for check blocked cdn
         let myHeaders = new HttpHeaders();
         myHeaders.append("Content-Type", "text/plain");
-        if ((window as any).cdnWorking)
+        if ((window as any).cdnWorking === true)
             this.cdn = environment.cdnURL;
         try {
             await this.insertLinks();
@@ -32,7 +30,29 @@ export class CDNCheck {
         console.log("loaded scripts");
     }
 
-
+    async insertElement(style: boolean, link) {
+        return new Promise(function (res, rej) {
+            let script = null;
+            if (style === true) {
+                script = document.createElement('link') as any;
+                script.href = link;
+                script.rel = 'stylesheet';
+            } else {
+                script = document.createElement('script') as any;
+                script.src = link;
+                script.type = 'text/javascript';
+            }
+            script.onError = rej;
+            script.async = true;
+            script.onload = res;
+            script.addEventListener('error', rej);
+            script.addEventListener('load', res);
+            if (style === true)
+                document.head.appendChild(script);
+            else
+                document.body.appendChild(script);
+        })
+    }
 
     async insertLinks() {
         let styles = [
@@ -46,8 +66,10 @@ export class CDNCheck {
 
         ];
         let scripts = [
-            "assets/cdn/jquery-3.2.1.min.js",
-            // "assets/cdn/select2.min.js",
+            // "assets/cdn/jquery-3.2.1.min.js",
+            "assets/cdn/bootstrap.bundle.min.js",
+            "assets/cdn/bigSlide.min.js",
+            "assets/cdn/select2.min.js",
             "assets/cdn/Chart.min.js",
             "assets/cdn/bootstrap-slider.min.js",
             "assets/cdn/jquery-swapsies.js",
@@ -57,18 +79,23 @@ export class CDNCheck {
 
         //Lets firt styles
         for (let st of styles) {
-            let _st = document.createElement("link");
-            _st.setAttribute("rel", "stylesheet");
-            _st.href = this.cdn + st;
-            document.head.appendChild(_st);
+            try {
+                await this.insertElement(true, this.cdn+ st);
+            }
+            catch (e) {
+                console.error(3);
+            }
+
         }
 
         //lets second script
         for (let sc of scripts) {
-            let _sc = document.createElement("script");
-            _sc.setAttribute("type", "text/javascript");
-            _sc.src = this.cdn + sc;
-            document.body.appendChild(_sc);
+            try {
+                await this.insertElement(false, this.cdn+ sc);
+            }
+            catch (e) {
+                console.error(e);
+            }
         }
 
     }
@@ -77,8 +104,9 @@ export class CDNCheck {
 
         let ready = function (re) {
             setTimeout(function () {
-                if ((window as any).cdnWorking === true || (window as any).cdnWorking === false)
+                if ((window as any).cdnWorking === true || (window as any).cdnWorking === false) {
                     re();
+                }
                 else
                     ready(re);
             }, 100);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,208 +1,208 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from'../services/product.service';
+import { ProductService } from '../services/product.service';
 import { Router, RouterLinkWithHref } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { CartService } from '../core/cart/cart.service';
-import { DomSanitizer} from '@angular/platform-browser';
-declare var jQuery:any;
+import { DomSanitizer } from '@angular/platform-browser';
+declare var jQuery: any;
 import { environment } from '../../environments/environment';
-import {IsLoginService} from '../core/login/is-login.service';
+import { IsLoginService } from '../core/login/is-login.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  products:any=[];
-  API:string=environment.apiURLImg;
-  cdnURL:string=environment.cdnURL;
-  role:any;
-  showLoading:boolean=true;
-  featuredSellers:any;
-  showError:boolean=false;
-  error:string;
-  logos=[];
-  featuredProducts:any;
-  images=[];
-  featuredTypesImages=[];
-  loadProduct:boolean=true;
-  featuredtypes:any;
-  fishTypeMenu:any;
-  fishTypeMenuImages=[];
-  fishTypeMenuImagesChild=[];
-  isLoggedIn:boolean = false;
-  menu:any = [];
+  products: any = [];
+  API: string = environment.apiURLImg;
+  cdnURL: string = environment.cdnURL;
+  role: any;
+  showLoading: boolean = true;
+  featuredSellers: any;
+  showError: boolean = false;
+  error: string;
+  logos = [];
+  featuredProducts: any;
+  images = [];
+  featuredTypesImages = [];
+  loadProduct: boolean = true;
+  featuredtypes: any;
+  fishTypeMenu: any;
+  fishTypeMenuImages = [];
+  fishTypeMenuImagesChild = [];
+  isLoggedIn: boolean = false;
+  menu: any = [];
   lang: any = "en";
 
 
-  constructor(private isLoggedSr: IsLoginService, private product:ProductService, 
-    private auth: AuthenticationService, private sanitizer:DomSanitizer, 
-    private cart:CartService,private router:Router) {
-      jQuery(document).ready(function () {
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  constructor(private isLoggedSr: IsLoginService, private product: ProductService,
+    private auth: AuthenticationService, private sanitizer: DomSanitizer,
+    private cart: CartService, private router: Router) {
+    jQuery(document).ready(function () {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         console.log("Es movil");
-        jQuery('video source').each(function(num,val){
-            jQuery(this).attr('src', '')
+        jQuery('video source').each(function (num, val) {
+          jQuery(this).attr('src', '')
         });
       }
-      });    
-
-  }
-
-  
-  async ngOnInit() {
-    
-  jQuery('body').addClass('home-header');
-  jQuery('#carouselExampleIndicators').carousel({
-		interval : false
-	});
-	var canScroll = 1;
-	function pauseScroll(){
-		console.log("GOT HERE");
-		console.log("CUrrent Scroll: ", canScroll);
-		canScroll = 1;
-	}
-
-    //FIREFOX
-    jQuery('#carouselExampleIndicators').bind('DOMMouseScroll', function(e){
-      e.stopImmediatePropagation();
-
-console.log("CanScroll: ", canScroll);
-	if (canScroll == 1) {
-	canScroll = 0;
-      console.log("Doing", e);
-      if(e.originalEvent.detail > 0) {
-        //scroll down
-        jQuery(this).carousel('next');
-	setTimeout(pauseScroll, 1300);
-    }else {
-        //scroll up
-        jQuery(this).carousel('prev');
-	setTimeout(pauseScroll, 1300);
-
-    }
-	}
-
-    //prevent page fom scrolling
-    return false;
-     
     });
 
-     //IE, Opera, Safari
- jQuery('#carouselExampleIndicators').bind('mousewheel', function(e){
-	console.log("Wheel Data: ", e.originalEvent.wheelDelta);
-   e.stopImmediatePropagation();
-  if(e.originalEvent.wheelDelta < -25) {
-      //scroll down
-      jQuery(this).carousel('next');
-  }else if (e.originalEvent.wheelDelta > 25){
-      //scroll up
-      jQuery(this).carousel('prev');
-
   }
 
-  //prevent page fom scrolling
-  return false;
-});
+
+  async ngOnInit() {
+    console.log(jQuery("body"));
+    jQuery('body').addClass('home-header');
+    jQuery('#carouselExampleIndicators').carousel({
+      interval: false
+    });
+    var canScroll = 1;
+    function pauseScroll() {
+      console.log("GOT HERE");
+      console.log("CUrrent Scroll: ", canScroll);
+      canScroll = 1;
+    }
+
+    //FIREFOX
+    jQuery('#carouselExampleIndicators').bind('DOMMouseScroll', function (e) {
+      e.stopImmediatePropagation();
+
+      console.log("CanScroll: ", canScroll);
+      if (canScroll == 1) {
+        canScroll = 0;
+        console.log("Doing", e);
+        if (e.originalEvent.detail > 0) {
+          //scroll down
+          jQuery(this).carousel('next');
+          setTimeout(pauseScroll, 1300);
+        } else {
+          //scroll up
+          jQuery(this).carousel('prev');
+          setTimeout(pauseScroll, 1300);
+
+        }
+      }
+
+      //prevent page fom scrolling
+      return false;
+
+    });
+
+    //IE, Opera, Safari
+    jQuery('#carouselExampleIndicators').bind('mousewheel', function (e) {
+      console.log("Wheel Data: ", e.originalEvent.wheelDelta);
+      e.stopImmediatePropagation();
+      if (e.originalEvent.wheelDelta < -25) {
+        //scroll down
+        jQuery(this).carousel('next');
+      } else if (e.originalEvent.wheelDelta > 25) {
+        //scroll up
+        jQuery(this).carousel('prev');
+
+      }
+
+      //prevent page fom scrolling
+      return false;
+    });
     console.log("Probando Home");
 
-   this.getLoginStatus();
-   this.isLoggedSr.role.subscribe((role:number)=>{
-    this.role=role
-    if( this.role === -1 )
-      this.isLoggedIn = false;
-  })
-   
+    this.getLoginStatus();
+    this.isLoggedSr.role.subscribe((role: number) => {
+      this.role = role
+      if (this.role === -1)
+        this.isLoggedIn = false;
+    })
+
   }
 
   public ngOnDestroy() {
     jQuery('body').removeClass('home-header');
   }
- 
-  getLoginStatus(){
+
+  getLoginStatus() {
     let login = this.auth.getLoginData();
-    console.log( 'login status', login );
-   if(login != null){
-     this.isLoggedIn = true;
-   } else {
-     this.isLoggedIn = false;
-   }
+    console.log('login status', login);
+    if (login != null) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
   }
-  getFeaturedProducts(){ 
+  getFeaturedProducts() {
     this.product.getData('featuredproducts').subscribe(
-      result=>{
-        this.featuredProducts=result;
+      result => {
+        this.featuredProducts = result;
         this.getImages();
       },
-      error=>{
+      error => {
         console.log(error);
-        this.showError=true;
-        this.error="No Product found"
+        this.showError = true;
+        this.error = "No Product found"
       }
     )
   }
-  getFeaturedSeller(){
+  getFeaturedSeller() {
     this.product.getData('featuredseller').subscribe(
-      result=>{
-        this.featuredSellers=result;
+      result => {
+        this.featuredSellers = result;
         this.getLogos();
       },
-      error=>{
+      error => {
         console.log(error);
-        this.showError=true;
-        this.error="No Seller found"
+        this.showError = true;
+        this.error = "No Seller found"
       }
     )
   }
-  getImages(){
-    let val=this.featuredProducts.length;
-    this.featuredProducts.forEach((data, index)=>{
+  getImages() {
+    let val = this.featuredProducts.length;
+    this.featuredProducts.forEach((data, index) => {
       this.product.getProductDetail(data.id).subscribe(result => {
-        this.products[index]=result;
-        if(result['imagePrimary'] && result['imagePrimary']!=''){
-          this.images[index]=this.sanitizer.bypassSecurityTrustStyle(`url(${this.API}${result['imagePrimary']})`)
+        this.products[index] = result;
+        if (result['imagePrimary'] && result['imagePrimary'] != '') {
+          this.images[index] = this.sanitizer.bypassSecurityTrustStyle(`url(${this.API}${result['imagePrimary']})`)
         }
-        else if(result['images'] && result['images'].length>0){
-          this.images[index]=this.sanitizer.bypassSecurityTrustStyle(`url(${this.API}${result['images'][0].src})`)
+        else if (result['images'] && result['images'].length > 0) {
+          this.images[index] = this.sanitizer.bypassSecurityTrustStyle(`url(${this.API}${result['images'][0].src})`)
         }
-        else{
-          this.images[index]=this.sanitizer.bypassSecurityTrustStyle("url(../../assets/default-img-product.jpg)")
+        else {
+          this.images[index] = this.sanitizer.bypassSecurityTrustStyle("url(../../assets/default-img-product.jpg)")
         }
-        if (val-1==index) {
-          this.loadProduct=false
+        if (val - 1 == index) {
+          this.loadProduct = false
         }
-      },error=>{
+      }, error => {
         console.log(error)
       })
     })
   }
-  getShortDesc(data){  
-    if(data.length>20){
-        let text=data.split(/\s+/).slice(0,20).join(" ")
-        return text+'...' 
+  getShortDesc(data) {
+    if (data.length > 20) {
+      let text = data.split(/\s+/).slice(0, 20).join(" ")
+      return text + '...'
     }
-    else{
+    else {
       return data
     }
   }
-  getLogos(){
-    this.featuredSellers.forEach((data, index)=>{
-      this.product.getData('store/'+data.id).subscribe(
-        result=>{
-          if(result['logo'] && result['logo']!=''){
-            this.logos[index]=this.API+result['logo'];
+  getLogos() {
+    this.featuredSellers.forEach((data, index) => {
+      this.product.getData('store/' + data.id).subscribe(
+        result => {
+          if (result['logo'] && result['logo'] != '') {
+            this.logos[index] = this.API + result['logo'];
           }
-          else{
-            this.logos[index]="../../assets/seafood-souq-seller-logo-default.png";
+          else {
+            this.logos[index] = "../../assets/seafood-souq-seller-logo-default.png";
           }
         },
-        error=>{
+        error => {
           console.log(error)
         }
       )
     })
   }
-  getFishTypeMenu(){
+  getFishTypeMenu() {
     // this.product.getData('featuredtypes-menu').subscribe(
     //   result=>{
     //     this.fishTypeMenu=result['featureds'];
@@ -236,42 +236,42 @@ console.log("CanScroll: ", canScroll);
     //   }
     // )
   }
-  getFeaturedTypes(){
+  getFeaturedTypes() {
     this.product.getData('featuredtypes/').subscribe(
-      result=>{
+      result => {
         console.log("res", result);
-        if(result.hasOwnProperty('featureds')){
-          this.featuredtypes=result['featureds'];
-          this.featuredtypes.forEach((data, index)=>{
-            if(data.images && data.images.length>0){
-              this.featuredTypesImages[index]=this.sanitizer.bypassSecurityTrustStyle(`url(${this.API}${data.images[0].src})`)
+        if (result.hasOwnProperty('featureds')) {
+          this.featuredtypes = result['featureds'];
+          this.featuredtypes.forEach((data, index) => {
+            if (data.images && data.images.length > 0) {
+              this.featuredTypesImages[index] = this.sanitizer.bypassSecurityTrustStyle(`url(${this.API}${data.images[0].src})`)
             }
-            else{
-              this.featuredTypesImages[index]=this.sanitizer.bypassSecurityTrustStyle("url(../../assets/default.jpg)")
+            else {
+              this.featuredTypesImages[index] = this.sanitizer.bypassSecurityTrustStyle("url(../../assets/default.jpg)")
             }
           })
-          jQuery(document).ready(function(){
+          jQuery(document).ready(function () {
             jQuery('#featuredtype2').appendTo('#featured1')
-          jQuery('#featured2').remove();
+            jQuery('#featured2').remove();
           })
         }
-        
+
       },
-      error=>{
+      error => {
         console.log(error)
       }
     )
   }
-  register(page){
-    this.router.navigate(['/register'],{ queryParams: { register: page } })
+  register(page) {
+    this.router.navigate(['/register'], { queryParams: { register: page } })
   }
 
-  next(){
+  next() {
     jQuery('#carouselExampleIndicators').carousel('next');
   }
 
-  prev(){
+  prev() {
     jQuery('#carouselExampleIndicators').carousel('prev');
   }
-  
+
 }
