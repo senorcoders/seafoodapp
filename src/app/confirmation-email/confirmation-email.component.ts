d3cb6e3d4de71179862b276c445ca4a571840076
= <=>
2019-06-28 10:16:55 -0600

    compuse conflictos

--- src/app/confirmation-email/confirmation-email.component.ts
+++ src/app/confirmation-email/confirmation-email.component.ts
@@ -1,38 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
-import {ProductService} from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
 @Component({
-  selector: 'app-confirmation-email',
-  templateUrl: './confirmation-email.component.html',
-  styleUrls: ['./confirmation-email.component.scss']
+	selector: 'app-confirmation-email',
+	templateUrl: './confirmation-email.component.html',
+	styleUrls: ['./confirmation-email.component.scss']
 })
 export class ConfirmationEmailComponent implements OnInit {
 
 	userId: any;
 	code: string;
-	verified: number = 3;
-  constructor(private route: ActivatedRoute, private product: ProductService) { }
+	verified: number = 0;
+	constructor(private route: ActivatedRoute, private product: ProductService) { }
 
- ngOnInit() {
-  	this.route.params.subscribe(params => {
-			this.userId = this.route.snapshot.params['userid'];
-			this.code = this.route.snapshot.params['code'];
+	ngOnInit() {
+		// this.route.params.subscribe(params => {
+		// 		this.userId = this.route.snapshot.params['userid'];
+		// 		this.code = this.route.snapshot.params['code'];
 
-			// this.product.updateData('user/'+this.userId, data).subscribe(
-			this.product.getData( `api/verification/${this.userId}/${this.code}` ).subscribe(
-				result => {
-					if ( result['message'] === 'valid' ) {
-						this.verified = 0;
-					} else {
-						this.verified = 1;
-					}
-				},
-				e => {
-					console.log(e);
-				}
-			);
-   	});
+		// 		// this.product.updateData('user/'+this.userId, data).subscribe(
+		// 		this.product.getData( `api/verification/${this.userId}/${this.code}` ).subscribe(
+		// 			result => {
+		// 				if ( result['message'] === 'valid' ) {
+		// 					this.verified = 0;
+		// 				} else {
+		// 					this.verified = 1;
+		// 				}
+		// 			},
+		// 			e => {
+		// 				console.log(e);
+		// 			}
+		// 		);
+		// });
 
-  }
+	}
 
 }
