a8d21d1f7147374f9fd051496d6d6da2881e75f6
dlara-ss <dayana@seafoodsouq.com>
2019-07-15 14:54:51 -0600

    Implementar cambios restantes en los inputs

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastrService } from '../toast.service';
 import { CountriesService } from '../services/countries.service';
 import { ProductService } from '../services/product.service';
+import { DomSanitizer } from '@angular/platform-browser';
 declare var jQuery: any;
 @Component({
   selector: 'app-add-product',
@@ -10,6 +11,8 @@ declare var jQuery: any;
   styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent implements OnInit {
+  @ViewChild('myInput', { static: true })
+  myInputVariable: ElementRef;
   productForm: FormGroup;
   name: FormControl;
   brand:FormControl;
@@ -30,7 +33,6 @@ export class AddProductComponent implements OnInit {
   minOrder: FormControl;
   maxOrder: FormControl;
   perBoxes: FormControl;
-  images:any = [];
   averageUnitWeight: FormControl;
   countries:any = [];
   countriesWithShipping:any = [];
@@ -44,10 +46,20 @@ export class AddProductComponent implements OnInit {
   public typeLevel3 = [];
   private productID = "";
   fishPreparation:any = [];
+  preparation: FormControl;
+  preparationChilds:any = [];
+  childPreparation: FormControl;
+  variationInfo:any = [];
+  showAverageUnit:boolean = false;
+  public staticmin: number = 1;
+  images:FormControl;
+  public imagesTmp = [];
+  imagesSend: FormControl;
+  deletedImages: FormControl;
+  private indexImage = 0;
 
- 
   constructor(private toast:ToastrService, private countryService: CountriesService,
-    private productService: ProductService) {}
+    private productService: ProductService, private sanitizer: DomSanitizer) {}
   ngOnInit() {
     this.createFormControl();
     this.RegisterProductForm();
@@ -79,9 +91,12 @@ export class AddProductComponent implements OnInit {
     this.minOrder = new FormControl('', [Validators.required]); 
     this.maxOrder = new FormControl('', [Validators.required]); 
     this.perBoxes = new FormControl(false, [Validators.required]); 
-    this.averageUnitWeight = new FormControl('', [Validators.nullValidator]); 
-
-
+    this.averageUnitWeight = new FormControl(10, [Validators.nullValidator]); 
+    this.preparation = new FormControl('', [Validators.required]); 
+    this.childPreparation = new FormControl('', [Validators.required]); 
+    this.images = new FormControl('', Validators.nullValidator);
+    this.imagesSend = new FormControl('', Validators.nullValidator);
+    this.deletedImages = new FormControl('', Validators.nullValidator);
 
   }
 
@@ -106,7 +121,12 @@ export class AddProductComponent implements OnInit {
       minOrder:this.minOrder,
       maxOrder: this.maxOrder,
       perBoxes: this.perBoxes,
-      averageUnitWeight: this.averageUnitWeight
+      averageUnitWeight: this.averageUnitWeight,
+      preparation: this.preparation,
+      childPreparation: this.childPreparation,
+      images: this.images,
+      imagesSend: this.imagesSend,
+      deletedImages: this.deletedImages
     });
 
   }
@@ -231,6 +251,16 @@ private getTreatment() {
 onChanges(): void {
   this.productForm.valueChanges.subscribe(val => {
     console.log("cambiando..", val);
+    if(val['perBoxes'] == true){
+      this.showAverageUnit = true;
+      if (val.averageUnitWeight > 0 && val.minOrder < val.averageUnitWeight) {
+        // this.product
+        val.minOrder = val.averageUnitWeight;
+        this.productForm.controls['minOrder'].setValue(val.averageUnitWeight);  
+      }
+    }else{
+      this.showAverageUnit = false;
+    }
   });
 }
 
@@ -336,5 +366,111 @@ updateProcess(selectedType){
       console.log(error);
     }
   );
+}
+
+setvariations(){
+  let mainCat = this.productForm.get('category').value;
+  let fishp = this.productForm.get('preparation').value;
+  this.productService.getData(`fishtype/${mainCat}/preparation/${fishp}/childs`).subscribe(res => {
+    console.log("Childs", res);
+    this.preparationChilds = res;
+  })
+}
+
+
+getKgs(){
+  let cat = this.productForm.get('category').value;
+  let preparation = this.productForm.get('childPreparation').value;
+  this.productService.getData(`fishvariations/type/${cat}/preparation/${preparation}`).subscribe(res  => {
+      console.log("KGS", res);
+      this.variationInfo = res['variationInfo'];
+  });
+}
+
+public onFileChange(event, i?) {
+  console.log(this.imagesTmp);
+  if (event.target.files && event.target.files.length) {
+    if (i !== undefined && i !== null) {
+      let file = event.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.imagesTmp[i] = { src: reader.result, type: this.imagesTmp[i].type };
+        this.reSavedImages();
+      };
+    } else {
+      for (let file of event.target.files) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+          let type = this.imagesTmp.length === 0 ? "primary" : "secundary";
+          this.imagesTmp.push({ src: reader.result, type });
+          // let arr = JSON.parse( this.productForm.get('deletedImages').value);
+          this.reSavedImages();
+        };
+      }
+    }
+
+  }
+}
+
+private reSavedImages() {
+  let data = this.imagesTmp.length === 0 ? '' : JSON.stringify(this.imagesTmp);
+  this.productForm.controls['imagesSend'].setValue(data);  
+  if (this.imagesTmp.length === 0) this.resetFiles();
+}
+
+private resetFiles() {
+  console.log(this.myInputVariable.nativeElement.files);
+  this.myInputVariable.nativeElement.value = "";
+  console.log(this.myInputVariable.nativeElement.files);
+}
+
+public toSl(to) {
+  let limit = { min: (this.imagesTmp.length - 4) * -1, max: 0 };
+  let index = JSON.parse(JSON.stringify({ ind: this.indexImage })).ind;
+  if (to < 0) {
+    index -= 1;
+  } else {
+    index += 1;
+  }
+  //verificamos que este entre los limites
+  if (limit.min > index || limit.max < index) return;
+  this.indexImage = index;
+}
+
+public inLimit(to) {
+  let limit = { min: (this.imagesTmp.length - 4) * -1, max: 0 };
+  let index = JSON.parse(JSON.stringify({ ind: this.indexImage })).ind;
+  if (to < 0) {
+    // index -= 1;
+    // console.log("-1", limit.min < index, limit.min, index);
+    return limit.min < index;
+  } else {
+    // index += 1;
+    // console.log("1", limit.max > index, limit.max, index);
+    return limit.max > index;
+  }
+}
+
+public calcLeft(i) {
+  if (this.indexImage === 0) return 0;
+  let width = $(".col-3.img-select").width();
+  if (this.indexImage < 0) {
+    let minus = (this.indexImage) * width;
+    return minus;
+  }
+
+  if (this.indexImage > 0) {
+    let maxis = (this.indexImage) * width;
+    return maxis;
+  }
+
+}
+
+public byPassImageUrl(image) {
+  return this.sanitizer.bypassSecurityTrustUrl(image);
 }
 }
